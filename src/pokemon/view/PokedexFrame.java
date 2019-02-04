e5bc670dbe624431107abc3ffd44b8dc4e0c42a7
Ethan M <guitarworthy365@gmail.com>
2019-02-04 09:51:29 -0700

    Tried to create pokedex frame class.

---
+++ src/pokemon/view/PokedexFrame.java
@@ -0,0 +1,13 @@
+package pokemon.view;
+import java.awt.event.ActionListener;
+
+import javax.swing.SpringLayout;
+
+import java.awt.event.ActionEvent;
+import javax.swing.*;
+import pokemon.controller.*;
+
+public class PokedexFrame 
+{
+	private 
+}
